5ff422f8b96f2607eb6c5a5c5dd9da32fd509575
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-03-10 20:18:59 +0100

    Initial

---
+++ OpenGL_GravitySim/src/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
+
+//resize window
+void framebuff_size_callback(GLFWwindow* window, int width, int height);
+
+//I/O handling
+void processInput(GLFWwindow* window);
+
+//load shaders
+std::string loadShaderSrc(const char* filename);
+
+
+
+int main() {
+	int success;
+	char infoLog[512];
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	//create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+	//init glad (rendering window)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	//resizing
+	glfwSetFramebufferSizeCallback(window, framebuff_size_callback);
+
+	/*
+		shaders
+	*/
+
+	//compile vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertShaderSrc = loadShaderSrc("src/shaders/vertex_core.glsl");
+	const GLchar* vertShader = vertShaderSrc.c_str();
+	glShaderSource(vertexShader, 1, &vertShader, NULL);
+	glCompileShader(vertexShader);
+
+	//vertex error handling
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error with vertex shader compilation: " << std::endl << infoLog << std::endl;
+	}
+
+
+	//compile fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string fragShaderSrc = loadShaderSrc("src/shaders/fragment_core.glsl");
+	const GLchar* fragShader = fragShaderSrc.c_str();
+	glShaderSource(fragmentShader, 1, &fragShader, NULL);
+	glCompileShader(fragmentShader);
+
+	//fragment error handling
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error with fragment shader compilation: " << std::endl << infoLog << std::endl;
+	}
+
+	//shader linking
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//shader error handling
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Failed to link shaders: " << std::endl << infoLog << std::endl;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		//render
+		glClearColor(0.8f, 0.6f, 0.1f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		processInput(window);
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuff_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+
+}
+
+std::string loadShaderSrc(const char* filename) {
+	std::ifstream file;
+	std::stringstream buf;
+
+	std::string ret = "";
+
+	file.open(filename);
+
+	if (file.is_open()) {
+		buf << file.rdbuf();
+		ret = buf.str();
+	}
+	else {
+		std::cout << "Could not open " << filename << std::endl;
+	}
+	file.close();
+
+	return ret;
+}
